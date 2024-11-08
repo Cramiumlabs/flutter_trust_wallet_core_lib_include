f85e961a5af0b23139af06fbfe10ffe5ae281dc7
Trung Nguyen <ngtritrung92@gmail.com>
2024-11-08 10:22:42 +0700

    Update lib dependency

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
+import 'package:flutter_trust_wallet_core/trust_wallet_core_ffi.dart';
 import 'package:flutter_trust_wallet_core_example/bitcoin_address_example.dart';
 import 'package:flutter_trust_wallet_core_example/bitcoin_transaction_example.dart';
 import 'package:flutter_trust_wallet_core_example/ethereum_example.dart';
@@ -30,13 +33,59 @@ class Example extends StatefulWidget {
 
 class _ExampleState extends State<Example> {
   late HDWallet wallet;
+  late PrivateKey masterKey;
 
   @override
   void initState() {
     FlutterTrustWalletCore.init();
     super.initState();
-    String mnemonic = "rent craft script crucial item someone dream federal notice page shrug pipe young hover duty"; // 有测试币的 tron地址
+    String mnemonic =
+        "rent craft script crucial item someone dream federal notice page shrug pipe young hover duty"; // 有测试币的 tron地址
     wallet = HDWallet.createWithMnemonic(mnemonic);
+    // wallet = HDWallet();
+
+    // List<int> numbers = [
+    //   138,
+    //   51,
+    //   59,
+    //   196,
+    //   104,
+    //   74,
+    //   12,
+    //   193,
+    //   100,
+    //   40,
+    //   192,
+    //   160,
+    //   165,
+    //   109,
+    //   67,
+    //   217,
+    //   14,
+    //   144,
+    //   98,
+    //   56,
+    //   152,
+    //   72,
+    //   175,
+    //   101,
+    //   55,
+    //   47,
+    //   236,
+    //   206,
+    //   129,
+    //   46,
+    //   26,
+    //   81
+    // ];
+    // Uint8List data = Uint8List.fromList(numbers);
+    // wallet = HDWallet.createWithData(data);
+
+    masterKey = wallet.getMaterKey(TWCurve.TWCurveSECP256k1);
+    wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum);
+    wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum);
+    // String data = masterKey.data().toString();
+    // print(data);
   }
 
   Widget _exampleItem({
@@ -61,6 +110,7 @@ class _ExampleState extends State<Example> {
         child: Column(
           children: [
             Text(wallet.mnemonic()),
+            Text(masterKey.data().toString()),
             Expanded(
               child: ListView(
                 padding: EdgeInsets.zero,
@@ -102,6 +152,7 @@ class _ExampleState extends State<Example> {
               onPressed: () async {
                 wallet.delete();
                 wallet = HDWallet();
+                masterKey = wallet.getMaterKey(TWCurve.TWCurveED25519);
                 setState(() {});
               },
               child: Text("gen"),
